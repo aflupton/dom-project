d3acd8d9c34223cf64ed4006ac8e3653c5c6f6e0
andrew lupton and shirley dennis <andrew@>
2018-03-20 17:13:51 -0700

    add css fixes

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
     $("ul#webpage").prepend("<li>Why hello there!</li>");
     $("ul#user").children("li").first().click(function(){
       (this).remove();
+    });
     $("ul#webpage").children("li").first().click(function(){
       (this).remove();
     });
@@ -12,10 +13,22 @@ $(document).ready(function() {
   $("button#goodbye").click(function() {
     $("ul#user").prepend("<li>Goodbye!</li>");
     $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
+    $("ul#user").children("li").first().click(function(){
+      (this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function(){
+      (this).remove();
+    });
   });
 
   $("button#stop").click(function() {
     $("ul#user").prepend("<li>Stop copying me!</li>");
     $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
+    $("ul#user").children("li").first().click(function(){
+      (this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function(){
+      (this).remove();
+    });
   });
 });
